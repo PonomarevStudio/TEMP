d9c78a58c9c1eb63cc80c4b726543a8ebd448ba9
PonomareVlad <Git@PonomareVlad.ru>
2022-02-28 18:47:20 +0500

    Updates

--- js/plans.mjs
+++ js/plans.mjs
@@ -1,8 +1,12 @@
 const switcher = document.getElementById('plans-switcher'),
     updateHash = () => location.hash = switcher.value,
+    getHashValue = () => location.hash ? location.hash.substring(1) : undefined,
     isOption = value => [...switcher.options].map(({value}) => value).includes(value),
-    updateSwitcher = (plan = location.hash ? location.hash.substring(1) : null) =>
-        updateHash(isOption(plan) ? switcher.value = plan : null)
-document.addEventListener("DOMContentLoaded", updateSwitcher)
-window.addEventListener('hashchange', updateSwitcher)
-switcher.addEventListener('change', updateHash)
+    updateSwitcher = (value = getHashValue()) => isOption(value) ? switcher.value = value : null
+
+window.addEventListener("load", () => {
+    updateSwitcher()
+    updateHash()
+    switcher.addEventListener('change', updateHash)
+    window.addEventListener('hashchange', updateSwitcher)
+})
