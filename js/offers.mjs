71b8c75fd1123459214e85bfd0e9bcd54fdcb541
PonomareVlad <Git@PonomareVlad.ru>
2022-03-02 14:03:52 +0500

    Offers data cache

--- js/offers.mjs
+++ js/offers.mjs
@@ -5,14 +5,16 @@ class Offers {
     }
 
     async loadOffers(url = this.options.url) {
+        if (sessionStorage.getItem('offers')) return this.loadCache()
         this.xml = await fetch(url, {mode: 'cors'}).then(r => r.text())
             .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
         this.list = [...offers.xml.querySelectorAll('offer')].map(offer => ({
             price: parseInt(offer.querySelector('price value').textContent),
             rooms: parseInt(offer.querySelector('rooms').textContent)
         }))
-        this[this.options.groupBy] = Object.fromEntries(Object.entries(this.groupOffers()).map(([name, group]) =>
+        this.groups = Object.fromEntries(Object.entries(this.groupOffers()).map(([name, group]) =>
             [name, {price: Math.min(...group.prices), ...group}]))
+        return this.saveCache()
     }
 
     groupOffers(offers = this.list, groupBy = this.options.groupBy) {
@@ -26,9 +28,18 @@ class Offers {
         return groups;
     }
 
+    saveCache() {
+        const {list, groups} = this
+        sessionStorage.setItem('offers', JSON.stringify({list, groups}))
+    }
+
+    loadCache() {
+        Object.assign(this, JSON.parse(sessionStorage.getItem('offers')))
+    }
+
     async get(group) {
         await this.loading
-        return this[this.options.groupBy][group]
+        return this.groups[group]
     }
 }
 
