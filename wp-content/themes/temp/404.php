ced50a18f57fe3b9d846a81ce33602e091153c6a
PonomareVlad <Git@PonomareVlad.ru>
2022-03-08 17:35:57 +0500

    Update

--- wp-content/themes/temp/404.php
+++ wp-content/themes/temp/404.php
@@ -5,6 +5,6 @@ get_header(); ?>
     <main class="documentation-text">
         <h1>Страница не найдена</h1>
         <br><br>
-        <a href="/">Вернуться на главную</a>
+        <a href="<?php bloginfo( 'url' ); ?>">Вернуться на главную</a>
     </main>
 <?php get_footer();
