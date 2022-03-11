38383221d7fb3b8563e26c2fcd87162f5e35331f
PonomareVlad <Git@PonomareVlad.ru>
2022-03-11 06:15:16 +0500

    Update

--- wp-content/themes/temp/template-parts/hero.php
+++ wp-content/themes/temp/template-parts/hero.php
@@ -1,7 +1,8 @@
+<?php $description = get_post_custom_values( 'hero_description' )[0] ?>
 <section class="hero">
-    <div class="content fixed-padding">
+    <div class="content">
         <h1><?= get_post_custom_values( 'hero_title' )[0] ?? the_title() ?></h1>
-        <p data-slider="text"><?php echo get_post_custom_values( 'hero_description' )[0] ?></p>
+		<?php if ( $description ): ?><p data-slider="text"><?= $description ?></p><?php endif; ?>
     </div>
     <img alt="" data-slider="img" loading="eager"
          src="<?= esc_url( get_post_custom_values( 'hero_image' )[0] ?? get_the_post_thumbnail_url( null, 'large' ) ) ?>">
