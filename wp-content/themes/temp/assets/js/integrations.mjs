fec2cb7690428843414e3ca009a78166b6c9d9ee
PonomareVlad <Git@PonomareVlad.ru>
2022-03-14 03:51:17 +0500

    Update

--- wp-content/themes/temp/assets/js/integrations.mjs
+++ wp-content/themes/temp/assets/js/integrations.mjs
@@ -1,21 +1,4 @@
 try {
-    (function (w, d, s, l, i) {
-        w[l] = w[l] || [];
-        w[l].push({
-            'gtm.start':
-                new Date().getTime(), event: 'gtm.js'
-        });
-        var f = d.getElementsByTagName(s)[0],
-            j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
-        j.async = true;
-        j.src =
-            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-        f.parentNode.insertBefore(j, f);
-    })(window, document, 'script', 'dataLayer', 'GTM-W7GGMPJ');
-} catch (e) {
-    console.error(e)
-}
-try {
     (function (d, w, k) {
         w.morekit_callback = function () {
             try {
@@ -35,6 +18,23 @@ try {
     console.error(e)
 }
 window.addEventListener('load', () => setTimeout(() => {
+    try {
+        (function (w, d, s, l, i) {
+            w[l] = w[l] || [];
+            w[l].push({
+                'gtm.start':
+                    new Date().getTime(), event: 'gtm.js'
+            });
+            var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+            j.async = true;
+            j.src =
+                'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+            f.parentNode.insertBefore(j, f);
+        })(window, document, 'script', 'dataLayer', 'GTM-W7GGMPJ');
+    } catch (e) {
+        console.error(e)
+    }
     try {
         (function (w, d, s, h, id) {
             w.roistatProjectId = id;
