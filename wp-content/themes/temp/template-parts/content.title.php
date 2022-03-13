ddc476169471f13bfc9fb8af0a0e8fb74c6892bb
PonomareVlad <Git@PonomareVlad.ru>
2022-03-14 03:39:52 +0500

    Update

--- wp-content/themes/temp/template-parts/content.title.php
+++ wp-content/themes/temp/template-parts/content.title.php
@@ -3,7 +3,6 @@
 if ( have_posts() ) {
 	while ( have_posts() ) {
 		the_post(); ?>
-        <br><br><br>
         <main class="documentation-text">
             <h1><?= get_post_custom_values( 'hero_title' )[0] ?? the_title() ?></h1>
             <br><br>
