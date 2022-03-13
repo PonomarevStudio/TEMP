783ad7083cb64edcf986c5106b7941344e2f7bdb
PonomareVlad <Git@PonomareVlad.ru>
2022-03-13 19:32:44 +0500

    Update

--- wp-content/themes/temp/footer.php
+++ wp-content/themes/temp/footer.php
@@ -1,5 +1,10 @@
 <?php get_template_part( 'template-parts/form' ); ?>
 <?php get_template_part( 'template-parts/footer' ); ?>
+<script>
+    const isStaticViewport = navigator.platform === 'iPhone' && navigator.userAgent.includes(" GSA/")
+    document.body.classList.toggle('static-viewport-height', isStaticViewport)
+    document.body.style.setProperty("--static-viewport-height", window.innerHeight + 'px');
+</script>
 <script src="https://unpkg.com/@webcomponents/webcomponentsjs/webcomponents-loader.js"></script>
 <?php wp_footer(); ?>
 </body>
