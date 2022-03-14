5ef8a2918f631e15afef09d0dc2ce40340ce12af
PonomareVlad <Git@PonomareVlad.ru>
2022-03-14 07:09:58 +0500

    Update

--- wp-content/themes/temp/footer.php
+++ wp-content/themes/temp/footer.php
@@ -5,7 +5,7 @@
     document.body.classList.toggle('static-viewport-height', isStaticViewport)
     document.body.style.setProperty("--static-viewport-height", window.innerHeight + 'px');
     window.addEventListener('load', () => setTimeout(() =>
-        document.querySelectorAll('img[loading="lazy" decoding="async"]').forEach(img => img.removeAttribute('loading')), 1))
+        document.querySelectorAll('img[loading="lazy"]').forEach(img => img.removeAttribute('loading')), 1))
 </script>
 <script src="https://unpkg.com/@webcomponents/webcomponentsjs/webcomponents-loader.js"></script>
 <?php wp_footer(); ?>
--- wp-content/themes/temp/template-parts/form.php
+++ wp-content/themes/temp/template-parts/form.php
@@ -3,21 +3,9 @@
     <div class="content">
         <h2>Остались вопросы?</h2>
         <hr>
+        <br>
         <p>Напишите нам</p>
-        <form action="/">
-            <input name="name" placeholder="Как к вам обращаться ?" required>
-            <input name="phone" placeholder="Телефон" required type="tel">
-            <input name="email" placeholder="E-mail" required type="email">
-            <p>Какой тип недвижимости интересует ?</p>
-            <div class="line">
-                <label class="line"><input checked name="type" type="radio" value="live"> Жилая</label>
-                <label class="line"><input name="type" type="radio" value="commercial"> Коммерческая</label>
-            </div>
-            <textarea name="question" placeholder="Ваш вопрос" required></textarea>
-            <label class="policy"><input checked name="policy" required type="checkbox">
-                Согласен на обработку персональных данных. <a href="#">Политика конфиденциальности</a>
-            </label>
-            <button type="submit">Отправить заявку</button>
-        </form>
+        <br>
+		<?= do_shortcode( '[contact-form-7 id="222" title="Заявки"]' ) ?>
     </div>
 </section>
