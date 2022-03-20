372939b333edd0051aae06588a1a4edbd2f751b3
PonomareVlad <Git@PonomareVlad.ru>
2022-03-21 03:01:03 +0500

    Update

--- wp-content/themes/temp/template-parts/map.php
+++ wp-content/themes/temp/template-parts/map.php
@@ -3,6 +3,6 @@
         <h2>Схема проезда</h2>
         <hr>
     </div>
-    <script type="text/javascript" charset="utf-8" async
-            src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A8209800f1a347a47d24f416bfd0083410c1655cef9d34004773d9e4c9b1a4520&amp;width=100%25&amp;height=500&amp;lang=ru_RU&amp;scroll=false"></script>
+    <iframe src="https://yandex.ru/map-widget/v1/-/CCUBqKhdOC" height="400" frameborder="1" allowfullscreen="true"
+            style="position:relative;width: 100%;border:0;"></iframe>
 </section>
